a038cd246c84b790085712a51fc8b239784680e0
Aadil Ansari <ansariaadil611@gmail.com>
2025-03-02 00:23:39 +0530

    basic config and Dockerfile

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+
+class ProdConfig(Config):
+    DEBUG = False
